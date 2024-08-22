e6ce155a9469282fe90101727577c38a61827fc2
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2024-08-22 20:55:34 +0500

    umare

--- database/src/app/components/Footer/page.jsx
+++ database/src/app/components/Footer/page.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <h1>Footer</h1>
+      <div className="Footer-Main">
+        <h1>Footer</h1>
+      </div>
     </>
   );
 };
