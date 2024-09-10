bc6407fd8c34f5f21bbdfedc741d1b7298dcf8f5
UmarAfzal <umerafzal981@gmail.com>
2024-09-10 19:11:11 +0500

    umar

--- database/src/app/components/Dash/Doctors/NewDoctorDetails/index.jsx
+++ database/src/app/components/Dash/Doctors/NewDoctorDetails/index.jsx
@@ -5,11 +5,12 @@ const NewDoctorDetails = () => {
     <>
       <div className="Add-Doctor-Details">
         <div className="Add-Doctor-flex">
-          <div>
+          <div className="img-flex">
             <img
               src="https://i.pinimg.com/564x/b4/e0/16/b4e016f63a4973233994c40bdeb30ede.jpg"
               alt=""
             />
+            <button>Upload Img</button>
           </div>
           <div className="fields-flex">
             <input type="text" placeholder="Name" required />
@@ -23,7 +24,7 @@ const NewDoctorDetails = () => {
               placeholder="Description"
               required
             ></textarea>
-            <button>Add Doctors</button>
+            <button className="add-doctor-btn">Add Doctors</button>
           </div>
         </div>
       </div>
