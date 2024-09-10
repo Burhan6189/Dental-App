22ebb6b61b5e3b004905042988dcf3b68fdc11c9
UmarAfzal <umerafzal981@gmail.com>
2024-09-10 20:36:29 +0500

    umar

--- database/src/app/components/Dash/Doctors/NewDoctorDetails/index.jsx
+++ database/src/app/components/Dash/Doctors/NewDoctorDetails/index.jsx
@@ -1,17 +1,15 @@
-'use client'
+"use client";
 import { CldUploadButton } from "next-cloudinary";
 import React, { useState } from "react";
 import { ImFilePicture } from "react-icons/im";
 
 const NewDoctorDetails = () => {
-
-
-  const [Image, setImage] =useState('');
-  const [Name, setname] =useState('');
-  const [Desigination, setDesigination] =useState('');
-  const [Location, setLocation] =useState('');
-  const [Department, setDepartment] =useState('');
-  const [Description, setDescription] =useState('');
+  const [Image, setImage] = useState("");
+  const [Name, setname] = useState("");
+  const [Desigination, setDesigination] = useState("");
+  const [Location, setLocation] = useState("");
+  const [Department, setDepartment] = useState("");
+  const [Description, setDescription] = useState("");
 
   return (
     <>
@@ -19,23 +17,52 @@ const NewDoctorDetails = () => {
         <div className="Add-Doctor-flex">
           <div className="img-flex">
             <img
-              src={Image ||"https://res.cloudinary.com/dgtk4rthy/image/upload/v1725977779/Dental/xgaxqrodvhinonj8teyy.jpg" }
+              src={
+                Image ||
+                "https://res.cloudinary.com/dgtk4rthy/image/upload/v1725977779/Dental/xgaxqrodvhinonj8teyy.jpg"
+              }
               alt=""
             />
-       
-             <CldUploadButton uploadPreset="Dentist-profile" onSuccess={(result)=>setImage(result.info.secure_url)}> Upload Image<ImFilePicture size={20}/> </CldUploadButton>
-            
+
+            <CldUploadButton
+              className="Upload-btn"
+              uploadPreset="Dentist-profile"
+              onSuccess={(result) => setImage(result.info.secure_url)}
+            >
+              Upload Image
+              <ImFilePicture size={20} />
+            </CldUploadButton>
           </div>
           <div className="fields-flex">
-            <input type="text" onChange={e=>setname(e.target.value)} placeholder="Name" required />
-            <input type="text" onChange={e=>setDesigination(e.target.value)} placeholder="Desigination" required />
-            <input type="text" onChange={e=>setDepartment(e.target.value)} placeholder="Department" required />
-            <input type="text" onChange={e=>setLocation(e.target.value)} placeholder="Location" required />
+            <input
+              type="text"
+              onChange={(e) => setname(e.target.value)}
+              placeholder="Name"
+              required
+            />
+            <input
+              type="text"
+              onChange={(e) => setDesigination(e.target.value)}
+              placeholder="Desigination"
+              required
+            />
+            <input
+              type="text"
+              onChange={(e) => setDepartment(e.target.value)}
+              placeholder="Department"
+              required
+            />
+            <input
+              type="text"
+              onChange={(e) => setLocation(e.target.value)}
+              placeholder="Location"
+              required
+            />
             <textarea
               name=""
               id=""
               rows={9}
-              onChange={e=>setDescription(e.target.value)}
+              onChange={(e) => setDescription(e.target.value)}
               placeholder="Description"
               required
             ></textarea>
