609c80f5cddbb5e75a0c5c8a56c87d2c078c7d95
UmarAfzal <umerafzal981@gmail.com>
2024-09-11 13:59:45 +0500

    umar

--- database/src/app/components/Dash/Doctors/AllDoctors/index.jsx
+++ database/src/app/components/Dash/Doctors/AllDoctors/index.jsx
@@ -9,144 +9,31 @@ const Doctors = () => {
       <div className="dash-items-main">
         <div className="All-Doctors-Main">
           <div className="All-Doctors-Flex">
-            <div className="Doctor-card">
-              <div className="content">
-                <img
-                  src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
-                  alt=""
-                />
-                <h4>Dr. Albert Miles</h4>
-                <p>1288 Natalie BrooK Apt. 966</p>
-                <div className="desigination">
-                  <h5>NUROLOGIST</h5>
+            <a href="/dash/doctors">
+              <div className="Doctor-card">
+                <div className="content">
+                  <img
+                    src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
+                    alt=""
+                  />
+                  <h4>Dr. Albert Miles</h4>
+                  <p>1288 Natalie BrooK Apt. 966</p>
+                  <div className="desigination">
+                    <h5>NUROLOGIST</h5>
+                  </div>
+                </div>
+                <div className="option-flex">
+                  <div className="flex-1">
+                    <FaStar />
+                    <h5>4.5</h5>
+                  </div>
+                  <div className="flex-2">
+                    <FaUserDoctor />
+                    <h5>About Me</h5>
+                  </div>
                 </div>
               </div>
-              <div className="option-flex">
-                <div className="flex-1">
-                  <FaStar />
-                  <h5>4.5</h5>
-                </div>
-                <div className="flex-2">
-                  <FaUserDoctor />
-                  <h5>About Me</h5>
-                </div>
-              </div>
-            </div>
-            <div className="Doctor-card">
-              <div className="content">
-                <img
-                  src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1724339543/Dental/uifaul8xygqr1ml0ba6d.png"
-                  alt=""
-                />
-                <h4>Dr. Albert Miles</h4>
-                <p>1288 Natalie BrooK Apt. 966</p>
-                <div className="desigination">
-                  <h5>NUROLOGIST</h5>
-                </div>
-              </div>
-              <div className="option-flex">
-                <div className="flex-1">
-                  <FaStar />
-                  <h5>4.5</h5>
-                </div>
-                <div className="flex-2">
-                  <FaUserDoctor />
-                  <h5>About Me</h5>
-                </div>
-              </div>
-            </div>
-            <div className="Doctor-card">
-              <div className="content">
-                <img
-                  src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1724339543/Dental/uifaul8xygqr1ml0ba6d.png"
-                  alt=""
-                />
-                <h4>Dr. Albert Miles</h4>
-                <p>1288 Natalie BrooK Apt. 966</p>
-                <div className="desigination">
-                  <h5>NUROLOGIST</h5>
-                </div>
-              </div>
-              <div className="option-flex">
-                <div className="flex-1">
-                  <FaStar />
-                  <h5>4.5</h5>
-                </div>
-                <div className="flex-2">
-                  <FaUserDoctor />
-                  <h5>About Me</h5>
-                </div>
-              </div>
-            </div>
-            <div className="Doctor-card">
-              <div className="content">
-                <img
-                  src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1724339543/Dental/uifaul8xygqr1ml0ba6d.png"
-                  alt=""
-                />
-                <h4>Dr. Albert Miles</h4>
-                <p>1288 Natalie BrooK Apt. 966</p>
-                <div className="desigination">
-                  <h5>NUROLOGIST</h5>
-                </div>
-              </div>
-              <div className="option-flex">
-                <div className="flex-1">
-                  <FaStar />
-                  <h5>4.5</h5>
-                </div>
-                <div className="flex-2">
-                  <FaUserDoctor />
-                  <h5>About Me</h5>
-                </div>
-              </div>
-            </div>
-            <div className="Doctor-card">
-              <div className="content">
-                <img
-                  src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1724339543/Dental/uifaul8xygqr1ml0ba6d.png"
-                  alt=""
-                />
-                <h4>Dr. Albert Miles</h4>
-                <p>1288 Natalie BrooK Apt. 966</p>
-                <div className="desigination">
-                  <h5>NUROLOGIST</h5>
-                </div>
-              </div>
-              <div className="option-flex">
-                <div className="flex-1">
-                  <FaStar />
-                  <h5>4.5</h5>
-                </div>
-                <div className="flex-2">
-                  <FaUserDoctor />
-                  <h5>About Me</h5>
-                </div>
-              </div>
-            </div>
-            <div className="Doctor-card">
-              <div className="content">
-                <img
-                  src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1724339543/Dental/uifaul8xygqr1ml0ba6d.png"
-                  alt=""
-                />
-                <h4>Dr. Albert Miles</h4>
-                <p>1288 Natalie BrooK Apt. 966</p>
-                <div className="desigination">
-                  <h5>NUROLOGIST</h5>
-                </div>
-              </div>
-              <div className="option-flex">
-                <div className="flex-1">
-                  <FaStar />
-                  <h5>4.5</h5>
-                </div>
-                <div className="flex-2">
-                  <FaUserDoctor />
-                  <h5>About Me</h5>
-                </div>
-              </div>
-            </div>
+            </a>
           </div>
         </div>
       </div>
