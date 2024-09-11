43f277202e75accf56e760bb0652119cc505cb90
burhan6189 <burhanshabbir6189@gmail.com>
2024-09-11 16:23:17 +0500

    bbb

--- database/src/app/components/Dash/Doctors/AllDoctors/index.jsx
+++ database/src/app/components/Dash/Doctors/AllDoctors/index.jsx
@@ -8,54 +8,79 @@ import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 /* =========== Icons =========== */
 
-const Doctors = () => {
+
+export const getServerSideProps = async () => {
+
+  const data = await fetch('/api/doctors');
+  const jsondata = await data.json();
+
+  return {
+    props: {
+      doctorsdata: jsondata
+    }
+  }
+}
+
+
+
+const Doctors = ({ doctorsdata }) => {
+  console.log(doctorsdata)
   return (
     <>
       <div className="dash-items-main">
         <div className="All-Doctors-Main">
           <div className="All-Doctors-Flex">
-            <div className="Doctor-card">
-              <a href="/dash/doctors">
-                <div className="content">
-                  <img
-                    src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
-                    alt=""
-                  />
-                  <h4>Dr. Albert Miles</h4>
-                  <p>1288 Natalie BrooK Apt. 966</p>
-                  <div className="desigination">
-                    <h5>NUROLOGIST</h5>
+
+
+            {doctorsdata?.map(items => (
+
+
+              <div className="Doctor-card">
+                <a href="/dash/doctors">
+                  <div className="content">
+                    <img
+                      src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
+                      alt=""
+                    />
+                    <h4>Dr. Albert Miles</h4>
+                    <p>1288 Natalie BrooK Apt. 966</p>
+                    <div className="desigination">
+                      <h5>NUROLOGIST</h5>
+                    </div>
+                  </div>
+                </a>
+
+                <div className="option-flex">
+                  <div className="flex-1">
+                    <FaStar />
+                    <h5>4.5</h5>
+                  </div>
+                  <div className="flex-2">
+                    <Popup
+                      trigger={
+                        <button className="button">
+                          <SlOptions />
+                        </button>
+                      }
+                      position="bottom center"
+                      closeOnDocumentClick
+                    >
+                      <div className="option">
+                        <FaEdit />
+                        <p>Edit</p>
+                      </div>
+                      <div className="option">
+                        <MdDelete />
+                        <p>Delete</p>
+                      </div>
+                    </Popup>
                   </div>
                 </div>
-              </a>
-
-              <div className="option-flex">
-                <div className="flex-1">
-                  <FaStar />
-                  <h5>4.5</h5>
-                </div>
-                <div className="flex-2">
-                  <Popup
-                    trigger={
-                      <button className="button">
-                        <SlOptions />
-                      </button>
-                    }
-                    position="bottom center"
-                    closeOnDocumentClick
-                  >
-                    <div className="option">
-                      <FaEdit />
-                      <p>Edit</p>
-                    </div>
-                    <div className="option">
-                      <MdDelete />
-                      <p>Delete</p>
-                    </div>
-                  </Popup>
-                </div>
               </div>
-            </div>
+            ))}
+
+
+
           </div>
         </div>
       </div>
