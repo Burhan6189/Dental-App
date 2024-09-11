b25084cb7c513b25fd5cada2ad2f0a300a28578a
UmarAfzal <umerafzal981@gmail.com>
2024-09-11 14:18:01 +0500

    umar

--- database/src/app/components/Dash/Doctors/AllDoctors/index.jsx
+++ database/src/app/components/Dash/Doctors/AllDoctors/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+
+/* =========== Icons =========== */
 import { FaUserDoctor } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa";
 import { FiPlus } from "react-icons/fi";
+/* =========== Icons =========== */
 
 const Doctors = () => {
   return (
@@ -9,6 +12,156 @@ const Doctors = () => {
       <div className="dash-items-main">
         <div className="All-Doctors-Main">
           <div className="All-Doctors-Flex">
+            <a href="/dash/doctors">
+              <div className="Doctor-card">
+                <div className="content">
+                  <img
+                    src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
+                    alt=""
+                  />
+                  <h4>Dr. Albert Miles</h4>
+                  <p>1288 Natalie BrooK Apt. 966</p>
+                  <div className="desigination">
+                    <h5>NUROLOGIST</h5>
+                  </div>
+                </div>
+                <div className="option-flex">
+                  <div className="flex-1">
+                    <FaStar />
+                    <h5>4.5</h5>
+                  </div>
+                  <div className="flex-2">
+                    <FaUserDoctor />
+                    <h5>About Me</h5>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <a href="/dash/doctors">
+              <div className="Doctor-card">
+                <div className="content">
+                  <img
+                    src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
+                    alt=""
+                  />
+                  <h4>Dr. Albert Miles</h4>
+                  <p>1288 Natalie BrooK Apt. 966</p>
+                  <div className="desigination">
+                    <h5>NUROLOGIST</h5>
+                  </div>
+                </div>
+                <div className="option-flex">
+                  <div className="flex-1">
+                    <FaStar />
+                    <h5>4.5</h5>
+                  </div>
+                  <div className="flex-2">
+                    <FaUserDoctor />
+                    <h5>About Me</h5>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <a href="/dash/doctors">
+              <div className="Doctor-card">
+                <div className="content">
+                  <img
+                    src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
+                    alt=""
+                  />
+                  <h4>Dr. Albert Miles</h4>
+                  <p>1288 Natalie BrooK Apt. 966</p>
+                  <div className="desigination">
+                    <h5>NUROLOGIST</h5>
+                  </div>
+                </div>
+                <div className="option-flex">
+                  <div className="flex-1">
+                    <FaStar />
+                    <h5>4.5</h5>
+                  </div>
+                  <div className="flex-2">
+                    <FaUserDoctor />
+                    <h5>About Me</h5>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <a href="/dash/doctors">
+              <div className="Doctor-card">
+                <div className="content">
+                  <img
+                    src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
+                    alt=""
+                  />
+                  <h4>Dr. Albert Miles</h4>
+                  <p>1288 Natalie BrooK Apt. 966</p>
+                  <div className="desigination">
+                    <h5>NUROLOGIST</h5>
+                  </div>
+                </div>
+                <div className="option-flex">
+                  <div className="flex-1">
+                    <FaStar />
+                    <h5>4.5</h5>
+                  </div>
+                  <div className="flex-2">
+                    <FaUserDoctor />
+                    <h5>About Me</h5>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <a href="/dash/doctors">
+              <div className="Doctor-card">
+                <div className="content">
+                  <img
+                    src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
+                    alt=""
+                  />
+                  <h4>Dr. Albert Miles</h4>
+                  <p>1288 Natalie BrooK Apt. 966</p>
+                  <div className="desigination">
+                    <h5>NUROLOGIST</h5>
+                  </div>
+                </div>
+                <div className="option-flex">
+                  <div className="flex-1">
+                    <FaStar />
+                    <h5>4.5</h5>
+                  </div>
+                  <div className="flex-2">
+                    <FaUserDoctor />
+                    <h5>About Me</h5>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <a href="/dash/doctors">
+              <div className="Doctor-card">
+                <div className="content">
+                  <img
+                    src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1725983016/re3cdyxnyvtnpuwyxwbs.jpg"
+                    alt=""
+                  />
+                  <h4>Dr. Albert Miles</h4>
+                  <p>1288 Natalie BrooK Apt. 966</p>
+                  <div className="desigination">
+                    <h5>NUROLOGIST</h5>
+                  </div>
+                </div>
+                <div className="option-flex">
+                  <div className="flex-1">
+                    <FaStar />
+                    <h5>4.5</h5>
+                  </div>
+                  <div className="flex-2">
+                    <FaUserDoctor />
+                    <h5>About Me</h5>
+                  </div>
+                </div>
+              </div>
+            </a>
             <a href="/dash/doctors">
               <div className="Doctor-card">
                 <div className="content">
--- database/src/app/components/Dash/Doctors/NewDoctorDetails/index.jsx
+++ database/src/app/components/Dash/Doctors/NewDoctorDetails/index.jsx
@@ -48,61 +48,63 @@ const NewDoctorDetails = () => {
 
   return (
     <>
-      <div className="Add-Doctor-Details">
-        <div className="Add-Doctor-flex">
-          <div className="img-flex">
-            <img
-              src={
-                Image ||
-                "https://res.cloudinary.com/dgtk4rthy/image/upload/v1725977779/Dental/xgaxqrodvhinonj8teyy.jpg"
-              }
-              alt=""
-            />
+      <div className="dash-items-main">
+        <div className="Add-Doctor-Details">
+          <div className="Add-Doctor-flex">
+            <div className="img-flex">
+              <img
+                src={
+                  Image ||
+                  "https://res.cloudinary.com/dgtk4rthy/image/upload/v1725977779/Dental/xgaxqrodvhinonj8teyy.jpg"
+                }
+                alt=""
+              />
 
-            <CldUploadButton
-              className="Upload-btn"
-              uploadPreset="Dentist-profile"
-              onSuccess={(result) => setImage(result.info.secure_url)}
-            >
-              <ImFilePicture size={20} />
-            </CldUploadButton>
-          </div>
-          <div className="fields-flex">
-            <input
-              type="text"
-              onChange={(e) => setname(e.target.value)}
-              placeholder="Name"
-              required
-            />
-            <input
-              type="text"
-              onChange={(e) => setDesigination(e.target.value)}
-              placeholder="Desigination"
-              required
-            />
-            <input
-              type="text"
-              onChange={(e) => setDepartment(e.target.value)}
-              placeholder="Department"
-              required
-            />
-            <input
-              type="text"
-              onChange={(e) => setLocation(e.target.value)}
-              placeholder="Location"
-              required
-            />
-            <textarea
-              name=""
-              id=""
-              rows={9}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="Description"
-              required
-            ></textarea>
-            <button onClick={adddoctor} className="add-doctor-btn">
-              Add Doctors
-            </button>
+              <CldUploadButton
+                className="Upload-btn"
+                uploadPreset="Dentist-profile"
+                onSuccess={(result) => setImage(result.info.secure_url)}
+              >
+                <ImFilePicture size={20} />
+              </CldUploadButton>
+            </div>
+            <div className="fields-flex">
+              <input
+                type="text"
+                onChange={(e) => setname(e.target.value)}
+                placeholder="Name"
+                required
+              />
+              <input
+                type="text"
+                onChange={(e) => setDesigination(e.target.value)}
+                placeholder="Desigination"
+                required
+              />
+              <input
+                type="text"
+                onChange={(e) => setDepartment(e.target.value)}
+                placeholder="Department"
+                required
+              />
+              <input
+                type="text"
+                onChange={(e) => setLocation(e.target.value)}
+                placeholder="Location"
+                required
+              />
+              <textarea
+                name=""
+                id=""
+                rows={9}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Description"
+                required
+              ></textarea>
+              <button onClick={adddoctor} className="add-doctor-btn">
+                Add Doctors
+              </button>
+            </div>
           </div>
         </div>
       </div>
