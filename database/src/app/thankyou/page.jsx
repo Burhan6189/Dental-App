2e43a4cbc930f80b1489b64f226ffb8e233eabdb
UmarAfzal <umerafzal981@gmail.com>
2024-09-13 20:16:39 +0500

    umar

--- database/src/app/thankyou/page.jsx
+++ database/src/app/thankyou/page.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const thankyou = () => {
   return (
     <>
-      <div>thankyou</div>
+      <div className="Thankyou-Page-Main">
+        <img
+          src="https://res.cloudinary.com/dgtk4rthy/image/upload/v1726239964/Dental/lkxh9uldgamhc11tmumc.png"
+          alt=""
+        />
+        <h3>Thanks for Booking Appointemt</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          saepe et quam eligendi optio saepe et quam eligendi optio saepe et
+          quam eligendi optio
+        </p>
+        <a href="/home">
+          <button>Back to Home</button>
+        </a>
+      </div>
     </>
   );
 };
