94abc1e3a14e65ced1adfbd0fa0e6acd70766ca9
burhan6189 <burhanshabbir6189@gmail.com>
2024-09-13 19:44:15 +0500

    burhan

--- database/src/app/calendar/page.jsx
+++ database/src/app/calendar/page.jsx
@@ -205,7 +205,7 @@ function MyCalendar(context) {
                 </div>
               )}
               <div className="Continue-Btn">
-                <button>Confirm........</button>
+                <button>Confirm</button>
                 <button
                   onClick={() => {
                     setselectedate("");
