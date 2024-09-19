7a665768ce2996da68b06342c7989b481b8f00a6
burhan6189 <burhanshabbir6189@gmail.com>
2024-09-19 19:32:13 +0500

    burhan

--- database/src/app/components/Dash/Calendar/index.jsx
+++ database/src/app/components/Dash/Calendar/index.jsx
@@ -61,6 +61,48 @@ const Calendar = () => {
         email: "courtney@example.com",
       },
     },
+    {
+      id: "2",
+      title: "Jerome Bell",
+      start: "2024-09-16T09:00:00",
+      end: "2024-09-16T11:00:00",
+      extendedProps: {
+        doctor: "Dr. Wade Warren",
+        imageUrl:
+          "https://res.cloudinary.com/dgtk4rthy/image/upload/c_crop,g_auto,h_200,w_200/Dental/uifaul8xygqr1ml0ba6d.jpg",
+        description: "Root Canal Treatment for Courtney Henry",
+        contact: "+123456789",
+        email: "courtney@example.com",
+      },
+    },
+    {
+      id: "3",
+      title: "Jerome Bell",
+      start: "2024-09-16T09:00:00",
+      end: "2024-09-16T11:00:00",
+      extendedProps: {
+        doctor: "Dr. Wade Warren",
+        imageUrl:
+          "https://res.cloudinary.com/dgtk4rthy/image/upload/c_crop,g_auto,h_200,w_200/Dental/uifaul8xygqr1ml0ba6d.jpg",
+        description: "Root Canal Treatment for Courtney Henry",
+        contact: "+123456789",
+        email: "courtney@example.com",
+      },
+    },
+    {
+      id: "4",
+      title: "Jerome Bell",
+      start: "2024-09-16T09:00:00",
+      end: "2024-09-16T11:00:00",
+      extendedProps: {
+        doctor: "Dr. Wade Warren",
+        imageUrl:
+          "https://res.cloudinary.com/dgtk4rthy/image/upload/c_crop,g_auto,h_200,w_200/Dental/uifaul8xygqr1ml0ba6d.jpg",
+        description: "Root Canal Treatment for Courtney Henry",
+        contact: "+123456789",
+        email: "courtney@example.com",
+      },
+    },
   ];
 
   const handleEventClick = (clickInfo) => {
